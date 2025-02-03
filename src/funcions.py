852eb25ec83a6ebe5f9790a4722fb57d8e280d08
Felip Lloret <felllojul@alu.edu.gva.es>
2025-02-03 09:25:20 +0100

    2 commit

--- src/funcions.py
+++ src/funcions.py
@@ -1,10 +1,10 @@
 import math
 
 
-def es_primo(num):
-    if num < 2:
+def es_primo(Num):
+    if Num < 2:
         return False
-    for n in range(2, math.floor(math.sqrt(num) + 1)):
-        if num % n == 0:
+    for n in range(2, math.floor(math.sqrt(Num) + 1)):
+        if Num % n == 0:
             return False
     return True
