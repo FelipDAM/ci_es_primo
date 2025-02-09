51e0489cbb5ac12170689c73e12cdf75bcafd2bd
Felip Lloret <felllojul@alu.edu.gva.es>
2025-02-09 11:41:04 +0100

    goat

--- test_src/test_funcions.py
+++ test_src/test_funcions.py
@@ -1,13 +1,13 @@
-from src.funcions import es_primo
+from src.funcions import eSPriMo
 
 def test_primo_1():
-    assert es_primo(1) == False
+    assert eSPriMo(1) == False
 
 def test_es_primo_numero_primo():
-    assert es_primo(2) == True
+    assert eSPriMo(2) == True
  
 def test_es_primo_negativo():
-    assert es_primo(-10) == False
+    assert eSPriMo(-10) == False
 
 def test_es_primo_numero_primo_mayor_2():
-    assert es_primo(29) == True
+    assert eSPriMo(29) == True
