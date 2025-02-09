1c5ec2b0530e51a1db57520f248f4368ae9d461c
Felip Lloret <felllojul@alu.edu.gva.es>
2025-02-09 11:50:40 +0100

    hola

--- test_src/test_funcions.py
+++ test_src/test_funcions.py
@@ -1,13 +1,19 @@
-from src.funcions import eSPriMo
+'''gay'''
+import src.funcions as es_primo
 
 def test_primo_1():
-    assert eSPriMo(1) == False
+    '''Tamare es un import'''
+    assert es_primo.es_primo(1) is False
 
 def test_es_primo_numero_primo():
-    assert eSPriMo(2) == True
- 
+    '''Tamare es un import'''
+    assert es_primo.es_primo(2) is True
+
 def test_es_primo_negativo():
-    assert eSPriMo(-10) == False
+    '''Tamare es un import'''
+    assert es_primo.es_primo(-10) is False
+
 
 def test_es_primo_numero_primo_mayor_2():
-    assert eSPriMo(29) == True
+    '''Tamare es un import'''
+    assert es_primo.es_primo(29) is True
